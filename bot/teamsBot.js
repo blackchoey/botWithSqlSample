4c4aa110667aebcaa6dac8412ea7e4658212c7da
Chaoyi Yuan <chyuan@microsoft.com>
2021-10-08 17:11:16 +0800

    step 2 add code to connect to sql

--- bot/teamsBot.js
+++ bot/teamsBot.js
@@ -2,6 +2,8 @@ const { TeamsActivityHandler, CardFactory, TurnContext} = require("botbuilder");
 const rawWelcomeCard = require("./adaptiveCards/welcome.json");
 const rawLearnCard = require("./adaptiveCards/learn.json");
 const ACData = require("adaptivecards-templating");
+const {loadConfiguration, DefaultTediousConnectionConfiguration} = require("@microsoft/teamsfx");
+const {Connection, Request} = require("tedious");
 
 class TeamsBot extends TeamsActivityHandler {
   constructor() {
@@ -29,9 +31,14 @@ class TeamsBot extends TeamsActivityHandler {
           break;
         }
         case "learn": {
-          this.likeCountObj.likeCount = 0;
-          const card = this.renderAdaptiveCard(rawLearnCard, this.likeCountObj);
-          await context.sendActivity({ attachments: [card] });
+          // this.likeCountObj.likeCount = 0;
+          // const card = this.renderAdaptiveCard(rawLearnCard, this.likeCountObj);
+          // await context.sendActivity({ attachments: [card] });
+          loadConfiguration();
+          const connection = await getSQLConnection();
+          const query = "select system_user as u, sysdatetime() as t";
+          const result = await execQuery(query, connection);
+          await context.sendActivities(result[0][0]);
           break;
         }
         /**
@@ -45,6 +52,48 @@ class TeamsBot extends TeamsActivityHandler {
       // By calling next() you ensure that the next BotHandler is run.
       await next();
     });
+
+    async function getSQLConnection() {
+      const sqlConnectConfig = new DefaultTediousConnectionConfiguration();
+      const config = await sqlConnectConfig.getConfig();
+      const connection = new Connection(config);
+      return new Promise((resolve, reject) => {
+        connection.on("connect", (error) => {
+          if (error) {
+            console.log(error);
+            reject(connection);
+          }
+          resolve(connection);
+        });
+      });
+    }
+    
+    async function execQuery(query, connection) {
+      return new Promise((resolve, reject) => {
+        const res = [];
+        const request = new Request(query, (err) => {
+          if (err) {
+            throw err;
+          }
+        });
+    
+        request.on("row", (columns) => {
+          const row = [];
+          columns.forEach((column) => {
+            row.push(column.value);
+          });
+          res.push(row);
+        });
+        request.on("requestCompleted", () => {
+          resolve(res);
+        });
+        request.on("error", () => {
+          console.error("SQL execQuery failed");
+          reject(res);
+        });
+        connection.execSql(request);
+      });
+    }
 
     // Listen to MembersAdded event, view https://docs.microsoft.com/en-us/microsoftteams/platform/resources/bot-v3/bots-notifications for more events
     this.onMembersAdded(async (context, next) => {
